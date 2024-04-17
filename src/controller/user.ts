061b2ba7cf37df193b1e48a74078281422f6e5e5
KevinDanudoro <kevindanudoro68@gmail.com>
2024-04-17 14:22:04 +0700

    fix: adding set cookies in signin controller

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -3,13 +3,24 @@
 import { responseSchema } from "@/model/respone";
 import { getUserSchema, signInUserSchema } from "@/model/user";
 import { signInUser } from "@/model/user/action";
+import { cookies } from "next/headers";
 import { z } from "zod";
 
 export const signInUserController = async (
   user: z.infer<typeof signInUserSchema>
 ) => {
   try {
-    const { data: response } = await signInUser(user);
+    const { data: response, headers } = await signInUser(user);
+
+    const authCookie = headers["set-cookie"]?.join().split(";")[0].split("=");
+    if (authCookie?.length === 2 && authCookie[0] === "Authorization")
+      cookies().set(authCookie[0], authCookie[1], {
+        expires: 1000 * 60 * 60,
+        maxAge: 1000 * 60 * 60,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      });
 
     const validResponse = responseSchema.safeParse(response);
     if (!validResponse.success) return null;
